0d86ac4278c3d890339a440de703ac8c1cf93874
Elnathan Dunia <ejdunia@gmail.com>
2021-04-13 01:13:43 +0100

    Added functions to get prefered user frequency and added mail sending function from othercode

--- email_reminder.py
+++ email_reminder.py
@@ -9,25 +9,39 @@ email_add = os.environ.get('MY_EMAIL')
 email_pass = os.environ.get('EMAIL_PASS')
 # have already had set os.environ files 
 
-
 print(f"Hi! this is a program that sends an email to you every hour reminding you to drink water.\nWater is LIFE!!!"
         "\nYour username will be the first part of your email")
 
+def get_freq():
+    while True:
+        try:
+            freq = int(input('Enter the frequency(in minutes) you want to be reminded: '))
+        except ValueError:
+            continue
+        return freq
+
+
+def send_message(subject, message, email, email_add=email_add, e_mesg=EmailMessage()):
+    """function to send the mail with  """
+    msg = e_mesg
+    msg['Subject'] = subject
+    msg['From'] = email_add
+    msg['To'] = email
+    msg.set_content(message)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_add, email_pass)
+        smtp.send_message(msg)
+
 user_email = input("Enter your email address: ")
+freq = get_freq()
+
 username = user_email.split('@')[0]
 now = datetime.now()
-print(now.strftime("%H:%M:%S"))
 
 while True:
     message = f"Hi! {username},\nThe time is {datetime.now().time()}, please take your water as recommended by the doctor.\nTodays Date: {date.today()} "
-    # msg = EmailMessage()
-    # msg['Subject'] = 'Python class test mail'
-    # msg['From'] = email_add
-    # msg['To'] = user_email0
-    # msg.set_content(message)
-    # with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    #     smtp.login(email_add, email_pass)
-    #     smtp.send_message(msg)
+    send_message(e_mesg=EmailMessage(), subject="Water Reminder", email_add=email_add, email=user_email, message=message)
     print(message)
-    time.sleep(60) # the arg is in seconds so 60 secs 
+    time.sleep(60 * freq) # the arg is in seconds so 60 secs 
 
